4c997ef05e41bcd03b1a02592fa0a83a6e3a20e2
arghyadeep99 <arghyadeep.d@somaiya.edu>
2021-08-26 13:34:02 +0530

    Updated Github data fetching method

--- src/portfolio.js
+++ src/portfolio.js
@@ -252,7 +252,6 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
   githubUserName: "arghyadeep99", // Change to your github username to view your profile in Contact Section.
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
   display: true // Set false to hide this section, defaults to true
