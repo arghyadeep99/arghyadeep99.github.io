92d19f045a0deb96f029d9680f3c5227dbaf0e1e
arghyadeep99 <arghyadeep.d@somaiya.edu>
2023-03-04 12:35:26 +0530

    Changed from react-lottie to lottie-react

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -1,19 +1,19 @@
 import React, {Component, Suspense} from "react";
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
 import Loading from "../../containers/loading/Loading";
 
 export default class DisplayLottie extends Component {
   render() {
     const animationData = this.props.animationData;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData
-    };
+    // const defaultOptions = {
+    //   loop: true,
+    //   autoplay: true,
+    //   animationData: animationData
+    // };
 
     return (
       <Suspense fallback={<Loading />}>
-        <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
+        <Lottie animationData={animationData} loop={true} autoplay={true} />
       </Suspense>
     );
   }
